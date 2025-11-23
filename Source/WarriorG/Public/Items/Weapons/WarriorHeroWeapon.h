d926cc8d28ac3063199e5ee22618fb8342f39172
Ghostory <ume6514@gmail.com>
2025-11-24 01:00:51 +0900

    Add WeaponClass

---
+++ Source/WarriorG/Public/Items/Weapons/WarriorHeroWeapon.h
@@ -0,0 +1,17 @@
+// Ghostory All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/Weapons/WarriorWeaponBase.h"
+#include "WarriorHeroWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIORG_API AWarriorHeroWeapon : public AWarriorWeaponBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/WarriorG/Public/Items/Weapons/WarriorWeaponBase.h
@@ -0,0 +1,30 @@
+// Ghostory All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WarriorWeaponBase.generated.h"
+
+
+class UBoxComponent;
+UCLASS()
+class WARRIORG_API AWarriorWeaponBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AWarriorWeaponBase();
+
+protected:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Weapons")
+	UStaticMeshComponent* WeaponMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapons")
+	UBoxComponent* WeaponCollisionBox;
+
+public:
+	FORCEINLINE UBoxComponent* GetWeaponCollisionBox() const { return WeaponCollisionBox; }
+};
