9ac816de8050fd26ecf12e91b0862b5059f8ce56
Ghostory <ume6514@gmail.com>
2026-02-07 02:42:26 +0900

    Add GA_Hero_EquipAxe New Function

--- Source/WarriorG/Private/AbilitySystem/WarriorAbilitySystemComponent.cpp
+++ Source/WarriorG/Private/AbilitySystem/WarriorAbilitySystemComponent.cpp
@@ -2,7 +2,7 @@
 
 
 #include "AbilitySystem/WarriorAbilitySystemComponent.h"
-
+#include "AbilitySystem/Abilities/WarriorGameplayAbility.h"
 void UWarriorAbilitySystemComponent::OnAbilityInputPressed(const FGameplayTag& InInputTag)
 {
 	if (!InInputTag.IsValid())
@@ -23,4 +23,25 @@ void UWarriorAbilitySystemComponent::OnAbilityInputPressed(const FGameplayTag& I
 void UWarriorAbilitySystemComponent::OnAbilityInputReleased(const FGameplayTag& InInputTag)
 {
 
+}
+
+void UWarriorAbilitySystemComponent::GrantHeroWeaponAbilities(const TArray<FWarriorHeroAbilitySet>& InDefaultWeaponAbilities, int32 ApplyLevel, TArray<FGameplayAbilitySpecHandle>& OutGrantedAbilitySpecHandle)
+{
+	if (InDefaultWeaponAbilities.IsEmpty())
+	{
+		return;
+	}
+
+	for (const FWarriorHeroAbilitySet& AbilitySet : InDefaultWeaponAbilities)
+	{
+		if (!AbilitySet.IsValid()) continue;
+
+		FGameplayAbilitySpec AbilitySpec(AbilitySet.AbilityToGrant);
+		AbilitySpec.SourceObject = GetAvatarActor();
+		AbilitySpec.Level = ApplyLevel;
+		AbilitySpec.DynamicAbilityTags.AddTag(AbilitySet.InputTag);
+		
+
+		OutGrantedAbilitySpecHandle.AddUnique(GiveAbility(AbilitySpec));
+	}
 }
