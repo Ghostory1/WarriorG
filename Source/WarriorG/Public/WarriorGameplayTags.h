5c112d2abe978b6caca8f3241a36b972c7f31ed4
Ghostory <ume6514@gmail.com>
2026-02-11 23:04:45 +0900

    adds combo system , slow motion, sound

--- Source/WarriorG/Public/WarriorGameplayTags.h
+++ Source/WarriorG/Public/WarriorGameplayTags.h
@@ -26,4 +26,6 @@ namespace WarriorGameplayTags
 
 	WARRIORG_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Event_Equip_Axe);
 	WARRIORG_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Event_Unequip_Axe);
+
+	WARRIORG_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Status_JumpToFinisher);
 }
